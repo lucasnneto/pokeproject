8ecb9a0e1c585d37422972ff86f10e37e210c91c
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-02-22 11:00:04 -0300

    nuxt config deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/pokeproject/'
+  },
   head: {
     title: 'pokeproject',
     htmlAttrs: {
